db7af9b52c2e820b84d7a8edf4bb3fb10e0c1adc
Debdutta <m-debdutta@users.noreply.github.com>
2023-05-11 18:02:42 +0530

    Refactored with different approach

--- src/calender.js
+++ src/calender.js
@@ -1,60 +1,72 @@
 const heading = function (month, year) {
-  return `${month} ${year}`;
+  const header = `${month} ${year}`;
+  const length = header.length;
+  const pad = 10 + Math.round(length / 2);
+
+  return header.padStart(pad);
 };
 
-const getNumberOfDays = function (month) {
-  const numberOfDays = 31;
-  return numberOfDays;
+const addPadding = function (times) {
+  let padding = new Array(times);
+  return padding.fill("  ");
 };
 
-const getDetails = function () {
-  const presentDate = new Date();
-  presentDate.setDate(1);
-  const [day, month, _, year] = presentDate.toDateString().split(" ");
-  return { day, month, year };
-};
-
-const generateLine = function (start, end) {
-  const line = [];
-  for (let index = start; index <= end; index++) {
-    line.push(index.toString().padStart(2));
+const getDates = function (endDate) {
+  let dates = new Array(endDate);
+  for (let index = 0; index < dates.length; index++) {
+    dates[index] = (index + 1).toString().padStart(2);
   }
-  return line;
+  return dates;
 };
 
-const makeCalender = function (dayCode, numberOfDays) {
-  let startDate = 1;
+const formatCalander = function (calenderData) {
   const calender = [];
-  let firstLine = new Array(dayCode);
-  firstLine.fill("  ");
-  let endDate = 7 - dayCode;
-  firstLine = firstLine.concat(generateLine(startDate, endDate));
-  calender.push(firstLine);
-  while (startDate <= numberOfDays) {
-    startDate = endDate + 1;
-    endDate = endDate + 7;
-    if (endDate > numberOfDays) {
-      endDate = numberOfDays;
-    }
-    const line = generateLine(startDate, endDate);
-    calender.push(line);
+  for (let index = 0; index < calenderData.length; index += 7) {
+    const week = calenderData.slice(index, index + 7);
+    calender.push(week);
   }
-
   return calender;
 };
 
-const main = function () {
-  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const makeCalender = function (inputMonth, inputYear) {
+  const presentDate = new Date();
 
-  const { day, month, year } = getDetails();
-  const numberOfDays = getNumberOfDays(month);
+  presentDate.setFullYear(inputYear);
+  presentDate.setMonth(inputMonth);
 
-  const code = days.indexOf(day);
-  const calender = makeCalender(code, numberOfDays);
+  presentDate.setDate(0);
+  const endDate = presentDate.getDate();
 
-  console.log(heading(month, year));
-  console.log("su mo tu we th fr sa");
+  presentDate.setDate(1);
+  const startDay = presentDate.getDay();
+
+  const dates = getDates(endDate);
+  const padding = addPadding(startDay);
+
+  const calender = padding.concat(dates);
+  return formatCalander(calender);
+};
+
+const main = function (month, year) {
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const calender = makeCalender(month, year);
+  const monthCode = 5 - 1;
+  console.log(heading(months[monthCode], year));
+  console.log("Su Mo Tu We Th Fr Sa");
   console.log(calender.join("\n").replaceAll(",", " "));
 };
 
-main();
+main(5, 3023);
