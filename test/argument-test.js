67e522c362545a76d377eb5b1580f7730a622482
Manjeet Sharma <manjeetsharma0796@users.noreply.github.com>
2023-05-13 00:20:37 +0530

    Implemented short hand arrow

--- test/argument-test.js
+++ test/argument-test.js
@@ -3,27 +3,27 @@ const { deepStrictEqual, strictEqual } = require("assert");
 
 const { isValidInput, parseArguments } = require("../src/arguments.js");
 
-describe('isValidInput', function() {
-	it('should give true when empty arguments is given', function() {
+describe('isValidInput', () => {
+	it('should give true when empty arguments is given', () => {
 		deepStrictEqual(isValidInput([]), true);
 	});
 
-	it('should give true when only numbers is passed as arguments', function() {
+	it('should give true when only numbers is passed as arguments', () => {
 		deepStrictEqual(isValidInput(['123']), true);
 	})
 
-it('should give false invalid is passed as arguments', function() {
+it('should give false invalid is passed as arguments', () => {
 		deepStrictEqual(isValidInput(['12some34']), false);
 	});
 });
 
-describe('parseArguments', function() {
-	it('should give isValid value as false on giving invalid arguments', function() {
+describe('parseArguments', () => {
+	it('should give isValid value as false on giving invalid arguments', () => {
 		const {isValid} = parseArguments(['13', '2000']);
 		strictEqual(isValid, false);
 	});
 
-	it('should give isValid value as true on giving valid arguments', function() {
+	it('should give isValid value as true on giving valid arguments', () => {
 		const {isValid} = parseArguments(['12', '2000']);
 
 		strictEqual(isValid, true);
--- test/calendar-test.js
+++ test/calendar-test.js
@@ -3,8 +3,8 @@ const { deepStrictEqual, strictEqual } = require("assert");
 
 const { getMonthName, getMonthInfo, chunkIntoWeeks, createPadding} = require("../src/calendar.js");
 
-describe("getMonthName", function () {
-	it("should give the week days", function () {
+describe("getMonthName",  () => {
+	it("should give the week days",  () => {
 		const month = 5;
 		const actual = getMonthName(month);
 		const expected = "May";
@@ -12,8 +12,8 @@ describe("getMonthName", function () {
 	});
 });
 
-describe("getMonthInfo", function () {
-	it("should give total days of the given month and year and the starting day of the month", function () {
+describe("getMonthInfo",  () => {
+	it("should give total days of the given month and year and the starting day of the month",  () => {
 		const actual = getMonthInfo(5, 2023);
 		const expected = { endDate: 31, startDay: 1 };
 
@@ -21,9 +21,9 @@ describe("getMonthInfo", function () {
 	});
 });
 
-describe('chunkIntoWeeks', function () {
+describe('chunkIntoWeeks',  () => {
 	
-	it('should give back days into chunks of 7days', function () {
+	it('should give back days into chunks of 7days',  () => {
 		const calendarData = [' 1', ' 2', ' 3', ' 4',' 5', ' 6', ' 7', ' 8', ' 9', ' 10', ' 11',' 12', ' 13', ' 14', ' 15'];
 		
 		const actual = chunkIntoWeeks(calendarData);
